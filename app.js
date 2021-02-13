af431f047e816384f1a8b6f52644705478de9e10
Rohit <48995037+rohit-vrrr@users.noreply.github.com>
2021-02-13 23:33:42 +0530

    Mailchimp API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 app.use(express.static("public"));
@@ -11,12 +12,50 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
-  console.log(firstName, lastName, email);
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  /* Mailchimp API reference */
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+  const jsonData = JSON.stringify(data);
+
+  /* Getting url and options field to request */
+  const url = "https://us1.api.mailchimp.com/3.0/lists/48dc6e275f";
+  const options = {
+    method: "POST",
+    auth: "rohit1:54d53975eab0d136a5f48b12405aaaa3-us1"
+  };
+
+  /* Requesting Mailchimp to send some data */
+  const request = https.request(url, options, function(response) {
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  /* Sending jsonData to Mailchimp */
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, function() {
   console.log("Listening on port 3000");
 });
+
+// API Key
+// 54d53975eab0d136a5f48b12405aaaa3-us1
+
+// List ID
+// 48dc6e275f
