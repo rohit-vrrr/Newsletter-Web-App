5319d10e5ab71f4e2b6db25f5e4d8fb27a387b98
Rohit <48995037+rohit-vrrr@users.noreply.github.com>
2021-02-13 18:55:41 +0530

    Added files
    + app.js
    + package.json

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.listen(3000, function() {
+  console.log("Listening on port 3000");
+});
